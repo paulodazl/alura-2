0c12500881a36086a4b6185895ddbd2907fef86a
paulodazl <paulogarciaquispe72@gmail.com>
2024-10-11 09:14:39 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'Qual seu jogo favorito? ',
+    'brawl stars'
+)
+
+criaCartao(
+    'pessoal',
+    'qual seu personagem de anime favorito?',
+    'escanor'
+)
+
+criaCartao(
+    'recomendação',
+    'um livro',
+    'as aventuras de iluminus odra'
+)
+
+criaCartao(
+    'recomendacão',
+    'um jogo',
+    'cuphead'
+)
